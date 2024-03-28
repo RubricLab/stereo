f4a2b067dd83219dcfa08859dd2f600913090063
DexterStorey <dexter@dexterstorey.com>
2024-03-28 15:13:16 -0400

    supa creds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 import {createClient} from '@supabase/supabase-js'
+import {env} from '~/env.mjs'
 
 export default function Page() {
 	// Create a single supabase client for interacting with your database
-	const supabase = createClient(
-		'https://xyzcompany.supabase.co',
-		'public-anon-key'
-	)
+	const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_API_KEY)
 
 	return (
 		<div className='flex h-screen w-full flex-col justify-center gap-10 p-5 sm:p-20'>
